b9b65de88651cdb89bffb562e6c47028239260c2
codegit2 <ameltwite7@gmail.com>
2025-07-13 21:24:29 +0100

    Début sur Java

---
+++ Parking/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+//        String[]  taskes = new String[] {"Lessiver", "Cuisiner", "Torchonner", "Balayer", "Répasser", "Ranger"};
+//
+//        for (String taske: taskes) System.out.println(taske);
+
+        int a = 0;
+
+        switch (a) {
+            case 7:
+                System.out.println("C'est sept");
+                break;
+            case 9:
+                System.out.println("C'est neuf");
+                break;
+            default:
+                System.out.println("Ce n'est ni sept ni neuf");
+
+
+        }
+    }
+}
